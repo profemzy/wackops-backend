e5a81077e8b074467d15278dcf2faf90328da461
Oladele Femi <profemzy@gmail.com>
2025-01-08 11:44:41 -0800

    Demo Configure OPENAI Azure

--- config/settings.py
+++ config/settings.py
@@ -22,9 +22,9 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
 
 # Configure Pusher (you can leave the last 2 settings alone).
-PUSHER_APP_ID = os.getenv("PUSHER_APP_ID", None)
-PUSHER_KEY = os.getenv("PUSHER_KEY", None)
-PUSHER_SECRET = os.getenv("PUSHER_SECRET", None)
+PUSHER_APP_ID = os.getenv("PUSHER_APP_ID", "123")
+PUSHER_KEY = os.getenv("PUSHER_KEY", "123")
+PUSHER_SECRET = os.getenv("PUSHER_SECRET", "123")
 PUSHER_CLUSTER = os.getenv("PUSHER_CLUSTER", "us2")
 PUSHER_SSL = True
 PUSHER_AUTH_ENDPOINT = "/api/auth/pusher/"
